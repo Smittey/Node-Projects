dae21c2f4534c5fd458d633c3db98bfef34ab8e2
Smittey <andy_smithy@hotmail.co.uk>
2016-07-18 00:35:45 +0100

    Changed tab indentation

--- npm-registry-publisher/index.js
+++ npm-registry-publisher/index.js
@@ -26,81 +26,81 @@ var fileName;
 if(process.argv.indexOf("-d") != -1)	
 {
     directory = process.argv[process.argv.indexOf("-d") + 1]; 
-	console.log("Traversing: " + directory);
+    console.log("Traversing: " + directory);
 }
 else if(process.argv.indexOf("--directory") != -1)	
 {
     directory = process.argv[process.argv.indexOf("--directory") + 1]; 
-	console.log("Traversing: " + directory);
+    console.log("Traversing: " + directory);
 }
 
 if(process.argv.indexOf("-r") != -1)
 {
     registry = process.argv[process.argv.indexOf("-r") + 1]; 
-	cmd += " --registry " + registry;
-	console.log("Using registry: " + registry);
+    cmd += " --registry " + registry;
+    console.log("Using registry: " + registry);
 }
 else if(process.argv.indexOf("--registry") != -1)
 {
     registry = process.argv[process.argv.indexOf("--registry") + 1]; 
-	cmd += " --registry " + registry;
-	console.log("Using registry: " + registry);
+    cmd += " --registry " + registry;
+    console.log("Using registry: " + registry);
 }
 else
 {
-	run = exec("npm config get registry", function(err, stdout, stderr) {
+    run = exec("npm config get registry", function(err, stdout, stderr) {
 
-		if(stdout)
-			console.log("Using registry: " + stdout);
-		if(err)
-			console.log(err);
-		if(stderr)
-			console.log(stderr);
-	});
+	if(stdout)
+            console.log("Using registry: " + stdout);
+	if(err)
+	    console.log(err);
+	if(stderr)
+	    console.log(stderr);
+    });
 }
 
 if(directory != undefined)
 {
-	walk(directory);
+    walk(directory);
 }
 else
 {
-	console.log("Error, no directory specified. Please do so using -d");
-	console.log("Example: node index.js -d /path/to/directory -r url:8081/nexus/content/repositories/npm/");
+    console.log("Error, no directory specified. Please do so using -d");
+    console.log("Example: node index.js -d /path/to/directory -r url:8081/nexus/content/repositories/npm/");
 }
 
 
 function walk(currentDirPath) 
 {
     fs.readdir(currentDirPath, function (err, files) {
-		if (err) 
+        if (err) 
             throw new Error(err);
 
         files.forEach(function (name) {
             filePath = path.join(currentDirPath, name);
             stat = fs.statSync(filePath);
 			
-			if (stat.isFile()) 
-			{
-				fileName = path.basename(filePath)
+            if (stat.isFile()) 
+	    {
+	        fileName = path.basename(filePath)
 				
-				if(fileName == "package.json")
-				{
-					console.log("Found module at: " + currentDirPath);
-					run = exec(cmd, {cwd: currentDirPath}, function(err, stdout, stderr) {
-
-						if(stdout)
-							console.log(stdout);
-						if(err)
-							console.log(err);
-						if(stderr)
-							console.log(stderr);
-					});
-				}
+		if(fileName == "package.json")
+		{
+		    console.log("Found module at: " + currentDirPath);
+		    run = exec(cmd, {cwd: currentDirPath}, function(err, stdout, stderr) {
+    
+     		       if(stdout)
+		            console.log(stdout);
+		        if(err)
+			    console.log(err);
+			if(stderr)
+			    console.log(stderr);
+		    });
+		}
             } 
-			else if (stat.isDirectory()) 
-			{
-	            walk(filePath);
+            else if (stat.isDirectory()) 
+	    {
+	        walk(filePath);
             }
         });
     });
